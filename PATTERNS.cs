1d00ab943707a5ae22656028b23fa6c69ec4e3d3
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2023-03-10 09:54:58 +0530

    Add project files.

---
+++ PATTERNS.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class patterns
+    {
+        int num1 { get; set; }
+        int num2 { get; set; }
+
+
+        public void NumberFromUser1()
+        {
+            Console.WriteLine("Please enter any number");
+            num1=Convert.ToInt32(Console.ReadLine());
+        }
+        public int NumberFromUser2() 
+        {
+            Console.WriteLine("Please Enter Any Number");
+            num2=Convert.ToInt32(Console.ReadLine());
+            return num2;
+        }
+        public static void Pattern1()
+        {
+          patterns PattObj= new patterns();
+            PattObj.NumberFromUser2();
+
+
+            for (int A = 1; A <= PattObj.num1; A++)
+            {
+                for( int B = 1; B<= PattObj.num1; B++)
+                {
+                    Console.WriteLine(B + " ");
+                }
+            }
+            
+        }
+        public void Pattern2() 
+        {
+            NumberFromUser1();
+
+            for (int E = num1; E >= 1; E--)
+            {
+                for (int F = num1; F >= 1; F--)
+                {
+                    Console.Write(" " + Convert.ToChar(64 + F));
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp4;
+
+namespace CallingFunctionsEx
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           // patterns.Pattern1();
+
+
+            //patterns pattobj1 = new patterns();
+
+          //  pattobj1.Pattern2();
+            pattern3();
+        }
+        public int num3 { get; set; }
+
+        public int  NumberFromUser3()
+        {
+            patterns pattobj2 = new patterns();
+            Console.WriteLine("Please enter any number ");
+          num3 = Convert.ToInt32(Console.ReadLine());
+            return num3;
+
+
+        }
+        public static void pattern3()
+        {
+            Program abcobj= new Program();
+            abcobj.NumberFromUser3();
+            for (int G = 1; G <= abcobj.num3; G++)
+            {
+                for (int H = 1; H <= abcobj.num3; H++)
+                {
+                    Console.Write(H + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+    }
+
+}
